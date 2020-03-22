c905919b6a3e8edbc9b25e0f45a6c7a3a3a0eb13
Cool Lin <andrzejkom2016@gmail.com>
2020-03-22 17:40:32 +0100

    initial stage

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace battle_of_cards_grupauderzeniowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
