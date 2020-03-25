4fa87342b27e85193cb2763c235a1a184c21b128
Cool Lin <andrzejkom2016@gmail.com>
2020-03-25 14:57:05 +0100

    ver1

--- Program.cs
+++ Program.cs
@@ -23,54 +23,6 @@ namespace battle_of_cards_grupauderzeniowa
             playerHand = talia.handDeck();
             playerHand.Sort(new CardComparer_Value());
             DisplayTable.DisplayBoard(dealerHand,playerHand, ante, call,current,result1, result2);
-            
-            /*Console.WriteLine("Dealer hand:");
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write(dealerHand[i].Name + " - " + dealerHand[i].Icon + " | ");
-            }
-            Console.WriteLine();
-            
-            dealerHand.Sort(new CardComparer_Value());
-            Console.WriteLine("Dealer hand sorted by value:");
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write(dealerHand[i].Name + " - " + dealerHand[i].Icon + " | ");
-            }
-            Console.WriteLine();
-            
-            dealerHand.Sort(new CardComparer_Suits());
-            Console.WriteLine("Dealer hand sorted by suits:");
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write(dealerHand[i].Name + " - " + dealerHand[i].Icon + " | ");
-            }
-            Console.WriteLine();
-            
-            playerHand = talia.handDeck();
-            Console.WriteLine("Player hand:");
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write(playerHand[i].Name + " - " + playerHand[i].Icon + " | ");
-            }
-            Console.WriteLine();
-            
-            playerHand.Sort(new CardComparer_Value());
-            Console.WriteLine("Player hand sorted by value:");
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write(playerHand[i].Name + " - " + playerHand[i].Icon + " | ");
-            }
-            Console.WriteLine();
-
-            playerHand.Sort(new CardComparer_Suits());
-            Console.WriteLine("Player hand sorted by suits:");
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write(playerHand[i].Name + " - " + playerHand[i].Icon + " | ");
-            }
-            Console.WriteLine();
-            Console.WriteLine();*/
         }
     }
 }
