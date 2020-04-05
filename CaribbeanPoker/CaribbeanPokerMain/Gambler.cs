492553ac6a598d1ffc4fad39697d3ace80d78004
zdulak <dpzdulski@gmail.com>
2020-04-05 13:12:59 +0200

    Cleaner structure of app. Controller class added.

--- CaribbeanPoker/CaribbeanPokerMain/Gambler.cs
+++ CaribbeanPoker/CaribbeanPokerMain/Gambler.cs
@@ -7,79 +7,12 @@ namespace CaribbeanPokerMain
     class Gambler : Hand
     {
         public int Money {get;set;}
-        public Gambler() => Money = 1000;
-        public void Quit()
+        public Controller TheController {get;} 
+        public Gambler()
         {
-            System.Environment.Exit(0);
-        }
-        public Ante GetAnte()
-        {
-            Ante ante = 0;
-            while(ante == 0) 
-            {
-                Console.WriteLine("Bet obligatory Ante or write quit in order to close the program.");
-                Console.Write("Possible values of the Ante: " );
-                foreach (var name in Enum.GetValues(typeof(Ante))) Console.Write((int)name + " ");
-                Console.Write("\n");
-                try
-                {
-                    var  input = Console.ReadLine();
-                    if (input.ToLower() == "quit") Quit();
-                    ante = (Ante)Enum.Parse(typeof(Ante), input);
-                }
-                catch(Exception)
-                {
-                    Console.WriteLine("You have entered an invalid value.");
-                    ante = 0;
-                }
-            } 
-            return ante;
-
-        }
-        public bool GetJackpot()
-        {
-            var input = "start";
-            while(input != "Y")
-            {
-                Console.WriteLine("Do  you want to participate in the jackpot? Y/N");
-                input = Console.ReadLine().ToUpper();
-                if (input == "Y")
-                {
-                    return true;
-                }
-                else if (input == "N")
-                {
-                    return false;  
-                }
-                else
-                {
-                    Console.WriteLine("You have entered an invalid value");
-                }
-            }
-            return true;
-        }
-        public bool GetCall()
-        {
-            var input = "start";
-            while(input != "Y")
-            {
-                Console.WriteLine("Do  you raise? Y/N");
-                input = Console.ReadLine().ToUpper();
-                if (input == "Y")
-                {
-                    return true;
-                }
-                else if (input == "N")
-                {
-                    return false;  
-                }
-                else
-                {
-                    Console.WriteLine("You have entered an invalid value");
-                }
-            }
-            return true;
-        }
+            Money = 1000;
+            TheController = new Controller();
+        } 
         public bool IsBroke() => Money <= 0;
     }
 }
