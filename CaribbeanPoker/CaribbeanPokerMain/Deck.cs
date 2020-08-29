7317bdb46199360a951f9de34734370765116bee
Damian Zdulski <dpzdulski@gmail.com>
2020-08-29 17:30:31 +0200

    Below zero money bug fixed. Controller refactored.

--- CaribbeanPoker/CaribbeanPokerMain/Deck.cs
+++ CaribbeanPoker/CaribbeanPokerMain/Deck.cs
@@ -5,38 +5,38 @@ namespace CaribbeanPokerMain
 {
     class Deck
     {
-        private readonly List<Card> cards;
+        private readonly List<Card> _cards;
         public Card CardBack {get;}
-        private readonly Random random;
+        private readonly Random _random;
         // Constructor creating a shuffled deck.
         public Deck()
         {
             var cardsDao = new CardsDao();
-            random = new Random();
-            cards = cardsDao.GetAllCards();
+            _random = new Random();
+            _cards = cardsDao.GetAllCards();
             Shuffle();
             CardBack = cardsDao.GetCard(0,0);
         }
         // Modern Fisher–Yates shuffle algorithm.
         public void Shuffle()
         {
-            for (int i = 0; i < (cards.Count - 2); ++i)
+            for (int i = 0; i < (_cards.Count - 2); ++i)
             {
-                int j = random.Next(i, cards.Count);
-                Card temp = cards[i];
-                cards[i] = cards[j];
-                cards[j] = temp;
+                int j = _random.Next(i, _cards.Count);
+                Card temp = _cards[i];
+                _cards[i] = _cards[j];
+                _cards[j] = temp;
             }
         }
         // Method returns a hand of cards from the beginning of the deck.
         public Card[] DequeueHand()
         {
-            var hand = cards.GetRange(0, 5).ToArray();
-            cards.RemoveRange(0, 5);
+            var hand = _cards.GetRange(0, 5).ToArray();
+            _cards.RemoveRange(0, 5);
             return hand;
         }
         // Method adds a hand of cards to the end of the deck.
-        public void EnqueueHand(Card[] hand) => cards.AddRange(hand);
+        public void EnqueueHand(Card[] hand) => _cards.AddRange(hand);
         
     }
 }
--- CaribbeanPoker/CaribbeanPokerMain/Game.cs
+++ CaribbeanPoker/CaribbeanPokerMain/Game.cs
@@ -26,16 +26,33 @@ namespace CaribbeanPokerMain
         public void Run()
         {
             _view.Clear();
-            while (!_gambler.IsBroke())
+            while (!_gambler.Wallet.IsBroke())
             {
-                _view.PrintStatus(_gambler.Money, _jackpot);
-                var ante = _gambler.TheController.GetAnte();
-                _gambler.Money -= (int)ante;
-                var isJackpot = _gambler.TheController.GetAnswer("Do  you want to participate in the jackpot?");
-                if (isJackpot) 
+                _view.PrintStatus(_gambler.Wallet.Money, _jackpot);
+                bool isAntePaid = false;
+                int ante = 0;
+                while (!isAntePaid)
                 {
-                    _jackpot += JackpotAnte;
-                    _gambler.Money -= (int)JackpotAnte;
+                    ante = _gambler.Controller.GetAnte();
+                    if (_gambler.Wallet.IsEnoughForAnte(ante))
+                    {
+                        _gambler.Wallet.Money -= ante;
+                        isAntePaid = true;
+                    }
+                    else
+                    {
+                        _view.PrintMsg("Not enough money in the wallet. Choose smaller ante.");
+                    }
+                }
+                bool isJackpot = false;
+                if (_gambler.Wallet.IsEnoughForJackpot(ante, JackpotAnte))
+                {
+                    isJackpot = _gambler.Controller.GetAnswer("Do  you want to participate in the jackpot?");
+                    if (isJackpot)
+                    {
+                        _jackpot += JackpotAnte;
+                        _gambler.Wallet.Money -= JackpotAnte;
+                    }
                 }
                 _gambler.Hand.Cards = _deck.DequeueHand();
                 _gambler.Hand.FlipCards(_gambler.Hand.Cards.Length, sorted: true, true);
@@ -43,9 +60,9 @@ namespace CaribbeanPokerMain
                 _dealer.Hand.FlipCards(1, sorted: false, true); // Reveal the dealer's first card 
                 _view.DisplayBoard(_dealer.Hand.Cards, _gambler.Hand.SortedCards, _deck.CardBack,
                     playerCombination: _gambler.Hand.GetHandCombination().ToString());
-                if (_gambler.TheController.GetAnswer("Do  you raise ?"))
+                if (_gambler.Controller.GetAnswer("Do  you raise ?"))
                 {
-                    _gambler.Money -= 2*(int)ante;
+                    _gambler.Wallet.Money -= 2*ante;
                     _dealer.Hand.FlipCards(_dealer.Hand.Cards.Length, sorted: true, true);
                     _view.DisplayBoard(_dealer.Hand.SortedCards, _gambler.Hand.SortedCards, _deck.CardBack,
                         _dealer.Hand.GetHandCombination().ToString(), _gambler.Hand.GetHandCombination().ToString());
@@ -54,14 +71,14 @@ namespace CaribbeanPokerMain
                         if (_gambler.Hand > _dealer.Hand)
                         {
                             _view.PrintMsg("You win!");
-                            _gambler.Money += 3*(int)ante; // return player money
-                            _gambler.Money += (int)ante;  // won dealer anteValues
-                            _gambler.Money += RankMoney(_gambler.Hand.GetHandCombination(), ante, isJackpot); // won bet bonus money
+                            _gambler.Wallet.Money += 3*ante; // return player money
+                            _gambler.Wallet.Money += ante;  // won dealer anteValues
+                            _gambler.Wallet.Money += RankMoney(_gambler.Hand.GetHandCombination(), ante, isJackpot); // won bet bonus money
                         }
                         else if (_dealer.Hand == _gambler.Hand)
                         {
                             _view.PrintMsg("Push!");
-                            _gambler.Money += 3*(int)ante;
+                            _gambler.Wallet.Money += 3*ante;
                         }
                         else
                         {
@@ -71,7 +88,7 @@ namespace CaribbeanPokerMain
                     else
                     {
                         _view.PrintMsg("Dealer folds!");
-                        _gambler.Money += 4*(int)ante;
+                        _gambler.Wallet.Money += 4*ante;
                     }
                 }
                 _dealer.Hand.FlipCards(_dealer.Hand.Cards.Length, sorted: false, false);
@@ -81,11 +98,11 @@ namespace CaribbeanPokerMain
                 _deck.Shuffle();
             }
             _view.PrintMsg("You are broke. Goodbye!");
-            _gambler.TheController.Quit();         
+            _gambler.Controller.Quit();         
         }
         private int RankMoney(HandCombination rank, int ante, bool isJackpot)
         {
-            int money = 2*(int)ante; // betted money
+            int money = 2*ante; // betted money
             switch (rank)
             {
                 case HandCombination.nothing:
