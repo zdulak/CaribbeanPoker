bf3f7dff0d29531db84b200617e260334d336767
Damian Zdulski <dpzdulski@gmail.com>
2020-09-05 15:18:11 +0200

    Main project folder name changed. Two tests of Hand class added

--- CaribbeanPoker/CaribbeanPoker.Test/HandShould.cs
+++ CaribbeanPoker/CaribbeanPoker.Test/HandShould.cs
@@ -1,23 +1,38 @@
 using System;
 using Xunit;
+using Xunit.Abstractions;
 using CaribbeanPokerMain;
+using System.Collections.Generic;
 
 namespace CaribbeanPoker.Test
 {
+    [Trait("Category", "Hand")]
     public class HandShould
     {
+        private readonly ITestOutputHelper _output;
         private readonly Hand _hand;
-        public HandShould(Hand hand)
+        public HandShould(ITestOutputHelper output)
         {
-            _hand = hand;
+            _output = output;
+            _hand = new Hand();
         }
         [Theory]
-        [InlineData()]
-        public void GetHandCombination_NoCombination_ReturnNothing(Card[] cards)
+        [MemberData(nameof(HandTestData.TestCardsNothing), MemberType = typeof(HandTestData))]
+        public void GetHandCombination_NoCombination_ReturnTheSame(Card[] cards)
         {
             _hand.Cards = cards;
-            var handCombination = _hand.GetHandCombination();
-            Assert.Equal(HandCombination.nothing, handCombination);
+            var actualCombination = _hand.GetHandCombination();
+            //_output.WriteLine("");
+            Assert.Equal(HandCombination.nothing, actualCombination);
+        }
+        [Theory]
+        [MemberData(nameof(HandTestData.TestCardsRoyalFlush), MemberType = typeof(HandTestData))]
+        public void GetHandCombination_RoyalFlush_ReturnTheSame(Card[] cards)
+        {
+            _hand.Cards = cards;
+            var actualCombination = _hand.GetHandCombination();
+            //_output.WriteLine("");
+            Assert.Equal(HandCombination.royal_flush, actualCombination);
         }
     }
 }
---
+++ CaribbeanPoker/CaribbeanPoker.Test/HandTestData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CaribbeanPokerMain;
+
+namespace CaribbeanPoker.Test
+{
+    class HandTestData
+    {
+        public static IEnumerable<object[]> TestCardsNothing
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new Card[]
+                    {
+                        new Card(Suit.Clubs, Rank.Two),
+                        new Card(Suit.Diamonds, Rank.Five),
+                        new Card(Suit.Hearts, Rank.Seven),
+                        new Card(Suit.Diamonds, Rank.Ace),
+                        new Card(Suit.Spades, Rank.King)
+                    }
+                };
+                yield return new object[]
+                {
+                    new Card[]
+                    {
+                        new Card(Suit.Clubs, Rank.Two),
+                        new Card(Suit.Diamonds, Rank.Jack),
+                        new Card(Suit.Hearts, Rank.Queen),
+                        new Card(Suit.Diamonds, Rank.Ace),
+                        new Card(Suit.Spades, Rank.King)
+                    }
+                };
+
+            }
+        }
+        public static IEnumerable<object[]> TestCardsRoyalFlush
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new Card[]
+                    {
+                        new Card(Suit.Spades, Rank.Ten),
+                        new Card(Suit.Spades, Rank.Jack),
+                        new Card(Suit.Spades, Rank.Queen),
+                        new Card(Suit.Spades, Rank.Ace),
+                        new Card(Suit.Spades, Rank.King)
+                    }
+                };
+            }
+        }
+    }
+}
